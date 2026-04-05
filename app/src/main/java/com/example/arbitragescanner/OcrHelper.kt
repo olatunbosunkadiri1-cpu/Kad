c6dcb7fb5370de4157d06979e8f0afd3a616b6c9
olatunbosunkadiri1-cpu <olatunbosunkadiri1@gmail.com>
2026-04-05 17:50:59 +0100

    Create OcrHelper.kt file

---
+++ app/src/main/java/com/example/arbitragescanner/OcrHelper.kt
@@ -0,0 +1,24 @@
+package com.example.arbitragescanner
+
+import android.graphics.Bitmap
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+object OcrHelper {
+    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    suspend fun extractText(bitmap: Bitmap): String = suspendCancellableCoroutine { continuation ->
+        val image = InputImage.fromBitmap(bitmap, 0)
+        recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                continuation.resume(visionText.text)
+            }
+            .addOnFailureListener { e ->
+                continuation.resumeWithException(e)
+            }
+    }
+}
